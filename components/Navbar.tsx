918d4f927b7f85462a1db640d94684179456117e
Elmer Jacobo <ejacobotiniano@gmail.com>
2022-04-02 22:23:39 -0500

    Pre-deployment Vercel

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import styles from './Navbar.module.css';
+import ActiveLink from "./ActiveLink";
+
+const menuItems = [
+    {
+        text: 'Home',
+        href: '/'
+    },
+    {
+        text: 'About',
+        href: '/about'
+    },
+    {
+        text: 'Contact',
+        href: '/contact'
+    },
+    {
+        text: 'Pricing',
+        href: '/pricing'
+    },
+];
+
+const Navbar = () => {
+    return (
+        <nav className={styles['menu-container']}>
+            {menuItems.map(({href, text}) => (
+                <ActiveLink key={href} href={href} text={text}/>
+            ))}
+        </nav>
+    );
+};
+
+export default Navbar;
